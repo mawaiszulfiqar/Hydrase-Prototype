ba2010e535c19dde3878d30f1dffe1c6d57b8d3a
Svetlin Pavlov <svetlin@Svetlins-MacBook-Pro.local>
2022-10-22 14:12:38 +0300

    adding level tiles and side menu navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
 import React from "react";
 import Unit from "./Components/Unit/Unit";
+import { IoLogoOctocat } from "react-icons/io";
+import { IoMdSettings } from "react-icons/io";
+import { TbFlame } from "react-icons/tb";
+import { BsFillCalendar2WeekFill } from "react-icons/bs";
+import { SlGraph } from "react-icons/sl";
 
 function App() {
   return (
-    // Doashboard Content Container
-    <div className="flex justify-center items-center m-5 flex-col">
-      {/* Unit Component */}
-      <Unit
-        UnitName="Unit 1"
-        UnitDescription="Basics of the German Language!"
-      />
-      <Unit UnitName="Unit 2" UnitDescription="Animal Names in German!" />
-      <Unit UnitName="Unit 3" UnitDescription="Friends, Food, and Fun!" />
-      <Unit UnitName="Unit 4" UnitDescription="Love, Life, and Laughter!" />
-    </div>
+    <>
+      <div className="flex">
+        <div className="fixed md:top-0 bottom-0 left-0 h-auto w-full md:h-screen md:w-16 m-0 flex md:flex-col justify-center items-center bg-[#202225] text-[#5865f2] shadow-lg z-50">
+          <div>
+            <IoLogoOctocat className="w-8 h-8 my-6 mx-5 cursor-pointer" />
+          </div>
+          <div>
+            <BsFillCalendar2WeekFill className="w-5 h-5 my-6 mx-5 cursor-pointer" />
+          </div>
+          <div>
+            <TbFlame className="w-6 h-6 my-6 mx-5 cursor-pointer" />
+          </div>
+          <div>
+            <SlGraph className="w-6 h-6 my-6 mx-5 cursor-pointer" />
+          </div>
+          <div className="md:mt-auto">
+            <IoMdSettings className="w-6 h-6 my-6 mx-5 cursor-pointer" />
+          </div>
+        </div>
+      </div>
+      {/* // Doashboard Content Container */}
+      <div className="flex justify-center items-center m-5 flex-col">
+        {/* Unit Component */}
+        <Unit
+          UnitName="Unit 1"
+          UnitDescription="Basics of the German Language!"
+        />
+        <Unit UnitName="Unit 2" UnitDescription="Animal Names in German!" />
+        <Unit UnitName="Unit 3" UnitDescription="Friends, Food, and Fun!" />
+        <Unit UnitName="Unit 4" UnitDescription="Love, Life, and Laughter!" />
+      </div>
+    </>
   );
 }
 
--- src/Components/Unit/Unit.tsx
+++ src/Components/Unit/Unit.tsx
@@ -1,10 +1,99 @@
 import React from "react";
 import { ReactComponent as UnitIcon } from "../../assets/Maps/de-arena.svg";
+import MapPointer from "../../assets/Maps/map_pointer.png";
 
 interface IUnitProps {
   UnitName: string;
   UnitDescription: string;
 }
+
+interface ILevelPointer {
+  id: number;
+  top: string;
+  left: string;
+}
+
+interface ILevelProps {
+  levelData: ILevelPointer[];
+}
+
+const DE_ARENA_PATHS = [
+  {
+    id: 1,
+    top: "3.8%",
+    left: "3%",
+  },
+  {
+    id: 2,
+    top: "17%",
+    left: "6%",
+  },
+  {
+    id: 3,
+    top: "20%",
+    left: "-25%",
+  },
+  {
+    id: 4,
+    top: "33%",
+    left: "-8%",
+  },
+  {
+    id: 5,
+    top: "40%",
+    left: "12%",
+  },
+  {
+    id: 6,
+    top: "55%",
+    left: "-8%",
+  },
+  {
+    id: 7,
+    top: "62%",
+    left: "-25%",
+  },
+  {
+    id: 8,
+    top: "68%",
+    left: "-6%",
+  },
+  {
+    id: 9,
+    top: "74%",
+    left: "20%",
+  },
+  {
+    id: 10,
+    top: "83%",
+    left: "12%",
+  },
+];
+
+const Level = ({ levelData }: ILevelProps) => {
+  return (
+    <>
+      {levelData.map((level) => (
+        <div
+          key={level.id}
+          className={`w-full flex select-none justify-center absolute top-[${level.top}] left-[${level.left}]`}
+        >
+          <div className="absolute text-white font-extrabold text-2xl top-[-10px] drop-shadow-xl group cursor-pointer group">
+            {level.id}
+            <div className="absolute top-5 left-2 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+              Level {level.id}
+            </div>
+          </div>
+          <img
+            src={MapPointer}
+            alt="map pointer"
+            className="w-full select-none pointer-events-none h-auto max-h-[24px] sm:max-h-[32px] object-contain"
+          />
+        </div>
+      ))}
+    </>
+  );
+};
 
 const Unit = ({ UnitName, UnitDescription }: IUnitProps) => {
   return (
@@ -14,8 +103,9 @@ const Unit = ({ UnitName, UnitDescription }: IUnitProps) => {
         <p className="text-yellow-400 font-bold text-center">
           {UnitDescription}
         </p>
-        <div className="w-full md:w-[576px]">
+        <div className="w-full md:w-[576px] min-w-[300px] relative">
           <UnitIcon className="w-full h-full rounded-3xl" />
+          <Level levelData={DE_ARENA_PATHS} />
         </div>
       </div>
     </div>
