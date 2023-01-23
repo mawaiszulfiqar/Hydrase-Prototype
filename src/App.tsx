af354c23a090b9c0369f88127908b36aeaca5a43
Svetlin <svetlinpav@gmail.com>
2023-01-23 20:44:16 +0200

    Improving structure, Adding Routing, Adding Sockets

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,18 @@
-import Unit from "./Components/Unit/Unit";
-// import { IoLogoOctocat } from "react-icons/io";
-// import { IoMdSettings } from "react-icons/io";
-// import { TbFlame } from "react-icons/tb";
-// import { BsFillCalendar2WeekFill } from "react-icons/bs";
-// import { SlGraph } from "react-icons/sl";
-import PageHeader from "./Components/PageHeader/PageHeader";
+import { Navigate, Route, Routes } from "react-router";
+import Learn from "./Pages/Learn/Learn";
+import SignIn from "./Pages/SignIn/SignIn";
+import { io } from "socket.io-client";
+
+export const socket = io("ws://localhost:9999");
 
 function App() {
+  const signedIn = false;
   return (
-    <>
-      {/* <div className="flex">
-        <div className="fixed md:top-0 bottom-0 left-0 h-auto w-full md:h-screen md:w-16 m-0 flex md:flex-col justify-center items-center bg-[#202225] text-[#5865f2] shadow-lg z-50">
-          <div>
-            <IoLogoOctocat className="w-8 h-8 my-6 mx-5 cursor-pointer" />
-          </div>
-          <div>
-            <BsFillCalendar2WeekFill className="w-5 h-5 my-6 mx-5 cursor-pointer" />
-          </div>
-          <div>
-            <TbFlame className="w-6 h-6 my-6 mx-5 cursor-pointer" />
-          </div>
-          <div>
-            <SlGraph className="w-6 h-6 my-6 mx-5 cursor-pointer" />
-          </div>
-          <div className="md:mt-auto">
-            <IoMdSettings className="w-6 h-6 my-6 mx-5 cursor-pointer" />
-          </div>
-        </div>
-      </div> */}
-      {/* // Doashboard Content Container */}
-      <PageHeader />
-      <div className="flex justify-center items-center m-5 flex-col">
-        {/* Unit Component */}
-        <Unit
-          UnitName="Unit 1"
-          UnitDescription="Basics of the German Language!"
-        />
-        <Unit UnitName="Unit 2" UnitDescription="Animal Names in German!" />
-        <Unit UnitName="Unit 3" UnitDescription="Friends, Food, and Fun!" />
-        <Unit UnitName="Unit 4" UnitDescription="Love, Life, and Laughter!" />
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Learn />} />
+      <Route path="/login" element={<SignIn />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
--- src/Components/Unit/Unit.tsx
+++ src/Components/Unit/Unit.tsx
@@ -1,5 +1,5 @@
 import { ReactComponent as UnitIcon } from "../../assets/Maps/de-arena.svg";
-
+import { BsBook } from "react-icons/bs";
 import LevelButton from "../LevelButton/LevelButton";
 
 import { CACTUS_VALLEY_PATH } from "../../utils/MapPaths/CactusValley";
@@ -11,11 +11,19 @@ interface IUnitProps {
 
 const Unit = ({ UnitName, UnitDescription }: IUnitProps) => {
   return (
-    <div className="flex flex-wrap overflow-hidden mt-8">
+    <div className="flex flex-wrap overflow-hidden mt-10 first:mt-0">
       <div className="w-full md:w-[576px] overflow-hidden">
-        <div className="bg-[#767e84] font-bold p-4 rounded-lg mb-[-28px] border-2 border-[#2F2F2F]">
-          <h2 className="text-white font-bold text-center">{UnitName}</h2>
-          <p className="text-white font-bold text-center mb-6">
+        <div className="bg-[#FAD850] font-bold p-4 rounded-lg mb-[-28px] border-2 border-[#2F2F2F]">
+          <div className="flex justify-between items-center">
+            <h2 className="text-black font-bold text-xl text-left tracking-wider">
+              {UnitName}
+            </h2>
+            <button className="flex items-center justify-center text-xs font-bold rounded-full px-4 py-1 space-x-1 bg-white text-black border-2 border-black hover:scale-105 transition-all duration-100">
+              <span className="text-sm mr-1">Textbook</span>
+              <BsBook className="w-5 h-5" />
+            </button>
+          </div>
+          <p className="text-black font-bold text-left mb-6">
             {UnitDescription}
           </p>
         </div>
