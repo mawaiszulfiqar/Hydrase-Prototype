543d6d9765357918c131d74cad5cf33c9ac905bb
Svetlin <svetlinpav@gmail.com>
2023-02-05 01:12:56 +0200

    Attempt to fix a warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import Providers from "./components/Providers/Providers";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
